1dd4639c719d7778ac3dd53b941f525b1a75f9c9
Tian <xtyao2015@gmail.com>
2024-12-20 11:41:49 -0500

    add connecting socket subclass

--- hdelibc/networking/ConnectingSocket.cpp
+++ hdelibc/networking/ConnectingSocket.cpp
@@ -1,1 +1,10 @@
 #include "ConnectingSocket.hpp"
+
+HDE::ConnectingSocket::ConnectingSocket(int domain, int service, int protocol, int port, u_long interface) : SimpleSocket{domain, service, protocol, port, interface} {
+    setConnection(connectToNetwork(getSock(), getAddress()));
+    testConnection(getConnection());
+};
+
+int HDE::ConnectingSocket::connectToNetwork(int sock, struct sockaddr_in address) {
+    return connect(sock, (struct sockaddr *)&address, sizeof(address));
+}
--- hdelibc/networking/ConnectingSocket.hpp
+++ hdelibc/networking/ConnectingSocket.hpp
@@ -1,6 +1,14 @@
 #ifndef CONNECTING_SOCKET
 #define CONNECTING_SOCKET
+#include <stdio.h>
+#include "SimpleSocket.hpp"
 
-
+namespace HDE {
+    class ConnectingSocket : public SimpleSocket {
+        public:
+        ConnectingSocket(int, int, int, int, u_long);
+        int connectToNetwork(int, struct sockaddr_in) override;
+    };
+};
 
 #endif

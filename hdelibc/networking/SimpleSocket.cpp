3d04ef924dfdd2afebf232fd8077f99f38d69e85
Tian <xtyao2015@gmail.com>
2024-12-20 11:14:20 -0500

    define socket class constructors, getters, and virtual method

---
+++ hdelibc/networking/SimpleSocket.cpp
@@ -0,0 +1,31 @@
+#include "SimpleSocket.hpp"
+
+HDE::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface) {
+    address.sin_family = domain;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(interface);
+
+    sock = socket(domain, service, protocol);
+    testConnection(sock);
+    connection = connectToNetwork(sock, address);
+    testConnection(connection);
+}
+
+void HDE::SimpleSocket::testConnection(int sockOrConnection) {
+    if (sockOrConnection < 0) {
+        perror("Connection failed broski");
+        exit(EXIT_FAILURE);
+    }
+}
+
+struct sockaddr_in HDE::SimpleSocket::getAddress() {
+    return address;
+}
+
+int HDE::SimpleSocket::getConnection() {
+    return connection;
+}
+
+int HDE::SimpleSocket::getSock() {
+    return sock;
+}
---
+++ hdelibc/networking/SimpleSocket.hpp
@@ -0,0 +1,25 @@
+#ifndef SIMPLE_SOCKET
+#define SIMPLE_SOCKET
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+
+namespace HDE {
+    class SimpleSocket {
+        struct sockaddr_in address;
+        int sock, connection;
+
+        public:
+        SimpleSocket(int, int, int, int, u_long);
+        virtual int connectToNetwork(int, struct sockaddr_in) = 0;
+        void testConnection(int);
+
+        struct sockaddr_in getAddress();
+        int getSock();
+        int getConnection();
+    };
+};
+
+#endif
